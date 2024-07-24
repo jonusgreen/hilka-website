e11a160c222e48959e5e1bf8920fcbe7cc34a517
jonus green <jonusashaba@gmail.com.com>
2024-07-24 16:00:54 +0300

    initi

---
+++ index.php
@@ -0,0 +1,473 @@
+
+
+<?php include("header.php");?>
+
+
+<div class="slider-section">
+
+
+<!-- revolution slider -->
+<section class="no-top no-bottom" aria-label="section-slider">
+<!-- home -->
+<div class="fullwidthbanner-container">
+<div id="revolution-slider-half">
+              <ul>
+
+                  <li data-transition="random" data-slotamount="10" data-masterspeed="1200" data-delay="5000">
+                      <!--  BACKGROUND IMAGE -->
+                      <img src="img/slider/slider_1.jpg" alt="" data-start="0" data-bgposition="center center" data-kenburns="on" data-duration="10000" data-ease="Linear.easeNone" data-bgfit="100" data-bgfitend="100" data-bgpositionend="center center"/>
+                      
+					  
+					   <div class="tp-caption slide_mask  sfl tp-resizeme "
+			data-x="0"
+			data-y="0" 
+			data-speed="500"
+			data-start="10"
+			data-easing="Power3.easeInOut"
+			data-splitin="none"
+			data-splitout="none"
+			data-elementdelay="0.1"
+			data-endelementdelay="0.1"
+			data-endspeed="300"
+			style=" max-width: auto; max-height: auto; white-space: nowrap;">
+		</div>
+					  
+					  <div class="tp-caption slide-big-heading sft"
+                          data-x="30"
+                          data-y="170"
+                          data-speed="800"
+                          data-start="400"
+                          data-easing="easeInOutExpo"
+                          data-endspeed="450">
+                        Hilka Construction Limited<BR> Building your Future Today!
+                      </div>
+					  
+					    <div class="tp-caption slide-sub-heading sft"
+                          data-x="30"
+                          data-y="320"
+                          data-speed="1000"
+                data-start="800"
+                data-easing="easeOutExpo"
+                          data-endspeed="400">
+                          Creating architectural and creative solutions  Creating architectural<BR> and creative solutions
+                      </div>
+
+                      <div class="tp-caption btn-slider sfb"
+                          data-x="30"
+                          data-y="390"
+                          data-speed="400"
+                          data-start="800"
+                          data-easing="easeInOutExpo">
+                          <span class="shine"></span><a href="#" >More Detail</a>
+                      </div>
+
+                  </li>
+                  
+                  <li data-transition="fade" data-slotamount="10" data-masterspeed="1200" data-delay="5000">
+                      <!--  BACKGROUND IMAGE -->
+                      <img src="img/slider/slider_2.jpg" alt="" data-start="0" data-bgposition="center center" data-kenburns="on" data-duration="10000" data-ease="Linear.easeNone" data-bgfit="100" data-bgfitend="100" data-bgpositionend="center center"/>
+                        
+						 <div class="tp-caption slide_mask  sfl tp-resizeme "
+			data-x="0"
+			data-y="0" 
+			data-speed="500"
+			data-start="10"
+			data-easing="Power3.easeInOut"
+			data-splitin="none"
+			data-splitout="none"
+			data-elementdelay="0.1"
+			data-endelementdelay="0.1"
+			data-endspeed="300"
+			style=" max-width: auto; max-height: auto; white-space: nowrap;">
+		</div>
+						
+						<div class="tp-caption slide-big-heading sft"
+                          data-x="30"
+                          data-y="170"
+                          data-speed="800"
+                          data-start="400"
+                          data-easing="easeInOutExpo"
+                          data-endspeed="450">
+                      Hilka Construction Limited<BR> Building your Future Today!
+                      </div>
+					  
+					    <div class="tp-caption slide-sub-heading sft"
+                          data-x="30"
+                          data-y="320"
+                          data-speed="1000"
+                data-start="800"
+                data-easing="easeOutExpo"
+                          data-endspeed="400">
+                          Creating architectural and creative solutions  Creating architectural<BR> and creative solutions
+                      </div>
+
+                      <div class="tp-caption btn-slider sfb"
+                          data-x="30"
+                          data-y="390"
+                          data-speed="400"
+                          data-start="800"
+                          data-easing="easeInOutExpo">
+                          <span class="shine"></span><a href="#" >More Detail</a>
+                      </div>
+
+                  </li>
+
+                 
+                 
+              </ul>
+              <div class="tp-bannertimer hide"></div>
+</div>
+</div>
+<!-- home end -->
+</section>
+<!-- revolution slider end -->
+    <div class="scroll"> <a href="#content2" class="go-down"></a></div>
+</div>
+
+
+
+	<section class="about-style-two py-5" id="content2">
+<div class="container">
+<div class="row align-items-center">
+<div class="col-lg-4">
+<div class="about-img">
+<img src="img/about-img1.png"  class="img-fluid" alt="about image">
+</div>
+</div>
+<div class="col-lg-8">
+<div class="about-text">
+<div class="section-title">
+<span>Our Company</span>
+<h2>Hilka Construction Limited Building your Future Today!</h2>
+</div>
+<p> At Hilka Construction Limited, we're dedicated to turning your vision into reality. With a team of experts and a commitment to excellence, 
+  we deliver top-notch construction services that exceed your expectations. From foundation to finish, we build your future with precision, passion, and professionalism.
+</p>
+
+<div class="d-flex">
+             
+
+                    <div class="logo_school mr-5">
+                        <img src="img/mission.svg" width="40">
+                        <h5>Mission</h5>
+						<p> "Hilka Construction Limited, our mission is to deliver exceptional construction services that meet our clients'
+               needs and exceed their expectations. We strive to build long-lasting relationships with our clients, partners, and team members through transparency, trust, and a commitment to excellence.
+            </p>
+                    </div>
+
+                    <div class="logo_school">
+                        <img src="img/vision.svg" width="40">
+                        <h5 class="text-left">
+                            Vision
+                           
+                        </h5>
+						<p>Our vision is to be the leading construction company in the region, renowned for our innovative approach, sustainability, and quality craftsmanship. We aim to create spaces that inspire, uplift, and enrich the lives of those who inhabit them, while contributing to the betterment of our communities and the environment.
+            </p>
+                    </div>
+                </div>
+</div>
+</div>
+</div>
+</div>
+</section>
+	
+	
+	
+	
+	<section class="service-style-three  pb-70">
+<div class="container">
+<div class="section-title text-center">
+<span>Our Services</span>
+<h2>Hilka Provides All Building Services</h2>
+<p>Ready to build your dream? Contact us today to schedule a consultation and let's get started on making your future a reality!
+</p>
+</div>
+<div class="row">
+<div class="col-lg-4 col-sm-6">
+<div class="service-card text-center">
+<div class="service-img urban">
+</div>
+<div class="service-content">
+  <img src="img/s1.svg"/>
+<h3>MASONRY</h3>
+<p>We are one of the best when it comes to providing residential and commercial customers with impressive brick and mortar hardscapes.</p>
+<a href="service-details.html" class="service-btn">
+Read More
+</a>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-sm-6">
+<div class="service-card text-center">
+<div class="service-img road">
+</div>
+<div class="service-content">
+  <img src="img/s2.svg"/>
+<h3>hardscaping</h3>
+<p>If you’re looking for a hardscape that makes an impression and gives you the function you need from your yard, we're here to help. We will help you create the perfect design for your 
+  home and give you a finished product that fits all your needs.</p>
+<a href="service-details.html" class="service-btn">
+Read More
+</a>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-sm-6">
+<div class="service-card text-center">
+<div class="service-img industrial">
+</div>
+<div class="service-content">
+  <img src="img/s3.svg"/>
+<h3>home improvement</h3>
+<p>Housing improvements are those that give us a different environment in our home,
+   we strive to give you the best solutions, we have our team of professionals when it comes to carrying out each of the different projects that by our experience we can carry out for our clients. we help you remodel from the inside to the outside of your home.</p>
+<a href="service-details.html" class="service-btn">
+Read More
+</a>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-sm-6">
+<div class="service-card text-center">
+<div class="service-img construction">
+</div>
+<div class="service-content">
+  <img src="img/s4.svg"/>
+<h3>Construction Engineering</h3>
+<p>With the help of our professional We deals with the designing, planning, construction, and management of infrastructures such as roads, tunnels, bridges, airports, railroads, facilities, buildings, dams, utilities and other projects.</p>
+<a href="service-details.html" class="service-btn">
+Read More
+</a>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-sm-6">
+<div class="service-card text-center">
+<div class="service-img structure">
+</div>
+<div class="service-content">
+  <img src="img/s5.svg"/>
+<h3>Structure Engineering</h3>
+<p>Through Structural engineering Trenty involves the analysis and design of structures such as buildings, bridges, towers, dams, tunnels, retaining walls and other infrastructure.</p>
+<a href="service-details.html" class="service-btn">
+Read More
+</a>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-sm-6">
+<div class="service-card text-center">
+<div class="service-img water">
+</div>
+<div class="service-content">
+  <img src="img/s6.svg"/>
+<h3>Water & Sewage Engineering</h3>
+<p>Trenty provides to clients the end use of Water may be drinking, industrial water supply, irrigation, river flow maintenance, water recreation or many other uses, including being safely returned to the environment.</p>
+<a href="service-details.html" class="service-btn">
+Read More
+</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+	
+	
+	
+	
+	<section class="facilities-section">
+<div class="container-fluid">
+<div class="row">
+<div class="col-lg-5 offset-lg-1 p-lg-0">
+<div class="facililties-text pt-100 pb-70">
+<div class="section-title">
+<span>More Facilities</span>
+<h2>Hilka Services You More</h2>
+<p>We strive for exceptional quality in everything we do
+2. and we operate with transparency, honesty, and ethical principles.
+</p>
+</div>
+<ul>
+<li>Best Quality Work</li>
+<li>Top Quality Equipment</li>
+<li>Experienced Workers</li>
+<li>Best Civil Engineers</li>
+<li>Budget-Friendly</li>
+<li>Great Understanding</li>
+<li>Emergency Support</li>
+<li>Responsible Reasonable</li>
+</ul>
+<div class="theme-btn">
+<a href="about.html" class="default-btn">
+Contact US
+</a>
+</div>
+</div>
+</div>
+<div class="col-lg-6 p-lg-0">
+<div class="facilities-img">
+</div>
+</div>
+</div>
+</div>
+</section>
+	
+	
+	<section class="project-section pt-100">
+<div class="container">
+<div class="section-title text-center">
+<span>Our Projects</span>
+<h2>We Have Some Completed Projects</h2>
+<p>These completed projects showcase Hilka Construction Limited's versatility, expertise, and commitment to delivering high-quality construction solutions across various sectors.</p>
+</div>
+ <div class="slick-slider project-slider">
+      <div>
+         <div class="gal-item"> 
+                <!-- GALLERY IMAGE --> 
+                <img src="img/img1.jpg" alt="" class="img-fluid"> 
+                <!-- Hover Info -->
+                <div class="hover-info">
+                  <h6>Construction</h6>
+                  <p> Maxius Ligula Semper</p>
+                  <a href="#."><i class="fas fa-arrow-right"></i></a> </div>
+              </div>  
+      </div>
+<div>
+       <div class="gal-item"> 
+                <!-- GALLERY IMAGE --> 
+                <img src="img/img3.jpg" alt="" class="img-fluid"> 
+                <!-- Hover Info -->
+                <div class="hover-info">
+                  <h6>Construction</h6>
+                  <p>Office block UfCC Buwama</p>
+                  <a href="#."><i class="fas fa-arrow-right"></i></a> </div>
+              </div>  
+      </div>
+<div>
+      <div class="gal-item"> 
+                <!-- GALLERY IMAGE --> 
+                <img src="img/img4.jpg" alt="" class="img-fluid"> 
+                <!-- Hover Info -->
+                <div class="hover-info">
+                  <h6>Construction</h6>
+                  <p> residential House Kyanja</p>
+                  <a href="#."><i class="fas fa-arrow-right"></i></a></div>
+              </div>
+      </div>
+<div>
+      <div class="gal-item"> 
+                <!-- GALLERY IMAGE --> 
+                <img src="img/img5.jpg" alt="" class="img-fluid"> 
+                <!-- Hover Info -->
+                <div class="hover-info">
+                  <h6>Construction</h6>
+                  <p> Maxius Ligula Semper</p>
+                  <a href="#."><i class="fas fa-arrow-right"></i> </div>
+              </div>
+      </div>
+ </div>
+
+<div class="project-btn text-center">
+<p>Visit Our All Projects. <a href="project.html">View More</a></p>
+<BR>
+</div>
+</div>
+</section>
+	
+	
+
+
+
+
+
+
+<section class="more-seciton">
+   <div class="container">
+     <div class="row">
+	   <div class="col-md-12">
+	      <div class="more-details m-0">
+		      <div class="section-title text-left m-0">
+<h2>Object & MOA</h2>
+</div>
+		       <P>Hilka Construction Limited aims to deliver high-quality projects on time and within budget,
+             exceeding client expectations, while embracing sustainability and innovation. We strive to build long-term relationships with clients through exceptional service, foster a culture of growth and development for our employees, and contribute to the betterment of urban communities. We prioritize health and safety, seek collaborative partnerships, and target business growth through strategic partnerships and marketing efforts. Ultimately, we aim to achieve industry recognition as a leader in urban construction, sustainability, and innovation, solidifying our reputation as a trusted and visionary construction company.</p>
+		  </div>
+	   </div>
+	 </div>
+   </div>
+</section>
+
+
+	
+	
+	<section class="client-section bggray py-5">
+<div class="container">
+    <div class="row">
+	  <div class="col-md-12">
+	    <div class="client-outer">
+	  <div class="slick-slider client-slider">
+          <div class="wow-outer">
+<div class="item wow slideInLeft"  data-wow-delay=".1s">
+            <img src="img/client/Exela.jpg" class="img-fluid" alt="Imageteam">
+        </div>
+				</div>
+				 <div class="wow-outer">
+<div class="item wow slideInLeft"  data-wow-delay=".3s">
+            <img src="img/client/6.jpeg" class="img-fluid" alt="Imageteam">
+        </div>
+				</div>
+				<div class="wow-outer">
+<div class="item wow slideInLeft" data-wow-delay=".5s">
+            <img src="img/client/logo_new_xxx.png" class="img-fluid" alt="Imageteam">
+        </div>
+				</div>
+					<div class="wow-outer">
+<div class="item wow slideInLeft" data-wow-delay=".7s">
+            <img src="img/client/4.jpg" class="img-fluid" alt="Imageteam">
+        </div>
+				</div>
+					<div class="wow-outer">
+             <div class="item wow slideInLeft" data-wow-delay=".9s">
+            <img src="img/client/5.jpeg" class="img-fluid" alt="Imageteam">
+        </div>
+				</div>
+				<div class="wow-outer">
+             <div class="item wow slideInLeft" data-wow-delay=".10s">
+            <img src="img/client/2.png" class="img-fluid" alt="Imageteam">
+        </div>
+				</div>
+				
+				<div class="wow-outer">
+             <div class="item wow slideInLeft" data-wow-delay=".1s">
+            <img src="img/client/7.png" class="img-fluid" alt="Imageteam">
+        </div>
+				</div>
+				
+				<div class="wow-outer">
+             <div class="item  wow slideInLeft" data-wow-delay=".3s">
+            <img src="img/client/8.png" class="img-fluid" alt="Imageteam">
+        </div>
+				</div>
+				
+            </div>
+	 
+	 </div>
+	</div>
+	
+ 
+           
+     
+	</div>
+</div>
+</section>
+
+
+
+
+
+
+
+
+<?php include("footer.php");?>
+	
